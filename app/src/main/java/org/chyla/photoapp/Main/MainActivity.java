e5f44db9cd6166edd8012ded57290351a8db80a3
Adam Chyła <adam@chyla.org>
2017-06-17 19:17:23 +0200

    allow switching fragments by changing its visibility

--- app/src/main/java/org/chyla/photoapp/Main/MainActivity.java
+++ app/src/main/java/org/chyla/photoapp/Main/MainActivity.java
@@ -3,9 +3,8 @@ package org.chyla.photoapp.Main;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,21 +12,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import org.chyla.photoapp.Login.LoginActivity;
 import org.chyla.photoapp.Main.Presenter.MainPresenter;
 import org.chyla.photoapp.Main.Presenter.MainPresenterImpl;
 import org.chyla.photoapp.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, MainView {
 
     MainPresenter presenter;
 
+    @BindView(R.id.gallery_fragment)
+    View galleryFragment;
+
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         presenter = new MainPresenterImpl(this);
 
@@ -43,7 +53,6 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
@@ -51,11 +60,12 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        hideAllFragments();
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -85,27 +95,22 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        hideAllFragments();
 
-        } else if (id == R.id.nav_slideshow) {
+        switch (id) {
+            case R.id.nav_gallery:
+                showGalleryFragment();
+                break;
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_logout) {
-            presenter.logoutUser();
+            case R.id.nav_logout:
+                presenter.logoutUser();
+                break;
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
@@ -113,6 +118,14 @@ public class MainActivity extends AppCompatActivity
     @Override
     public void startLoginActivity() {
         startActivity(new Intent(this, LoginActivity.class));
+    }
+
+    private void showGalleryFragment() {
+        galleryFragment.setVisibility(View.VISIBLE);
+    }
+
+    private void hideAllFragments() {
+        galleryFragment.setVisibility(View.GONE);
     }
 
 }

e9b9728f2a3bdae1d3e4fbff1407f6411e4ed62c
Adam Chyła <adam@chyla.org>
2017-06-17 18:03:09 +0200

    add empty gallery fragment

---
+++ app/src/main/java/org/chyla/photoapp/Main/GalleryFragment.java
@@ -0,0 +1,40 @@
+package org.chyla.photoapp.Main;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.chyla.photoapp.R;
+
+public class GalleryFragment extends Fragment {
+
+    public GalleryFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_gallery, container, false);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+}

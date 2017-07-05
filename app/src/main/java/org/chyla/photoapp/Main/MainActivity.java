3878589a0a2b5b8f0dfb1452a5d09b195c945a7a
Adam Chyła <adam@chyla.org>
2017-07-05 21:33:31 +0200

    add request to take a photo

--- app/src/main/java/org/chyla/photoapp/Main/MainActivity.java
+++ app/src/main/java/org/chyla/photoapp/Main/MainActivity.java
@@ -1,13 +1,22 @@
 package org.chyla.photoapp.Main;
 
+import android.Manifest;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -21,29 +30,35 @@ import android.widget.FrameLayout;
 
 import org.chyla.photoapp.Login.LoginActivity;
 import org.chyla.photoapp.Main.Configuration.FlickrPropertyReader;
+import org.chyla.photoapp.Main.InspectPhotos.GalleryFragment.GalleryFragment;
 import org.chyla.photoapp.Main.InspectPhotos.PhotoPreviewFragment.PhotoPreviewActionListener;
 import org.chyla.photoapp.Main.InspectPhotos.PhotoPreviewFragment.PhotoPreviewFragment;
 import org.chyla.photoapp.Main.InspectPhotos.SearchPhotosFragment;
 import org.chyla.photoapp.Main.Model.InspectPhotosInteractor;
 import org.chyla.photoapp.Main.Model.InspectPhotosInteractorImpl;
 import org.chyla.photoapp.Main.Model.objects.Photo;
-import org.chyla.photoapp.Main.InspectPhotos.GalleryFragment.GalleryFragment;
 import org.chyla.photoapp.Main.Presenter.MainPresenter;
 import org.chyla.photoapp.Main.Presenter.MainPresenterImpl;
 import org.chyla.photoapp.Main.Repository.FlickrRepository;
 import org.chyla.photoapp.Main.Repository.InspectPhotosRepository;
 import org.chyla.photoapp.R;
 
+import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, MainView, PhotoPreviewActionListener {
+        implements NavigationView.OnNavigationItemSelectedListener, MainView, PhotoPreviewActionListener, ActivityCompat.OnRequestPermissionsResultCallback {
 
     private final static String LOG_TAG = "MainActivity";
+    private final static int REQUEST_IMAGE_CAPTURE = 10;
+    private final static int PERMISSIONS_REQUEST_PHOTO = 20;
 
     MainPresenter presenter;
 
@@ -137,6 +152,10 @@ public class MainActivity extends AppCompatActivity
                 showGalleryFragment();
                 break;
 
+            case R.id.nav_camera:
+                takePhoto();
+                break;
+
             case R.id.nav_inspect_photos:
                 showSearchPhotosFragment();
                 break;
@@ -175,6 +194,86 @@ public class MainActivity extends AppCompatActivity
         fragment.setPhoto(photo);
         fragment.setPhotoActionListener(this);
         showFragment(fragment);
+    }
+
+    private void takePhoto() {
+        Log.i(LOG_TAG, "SDK Version: " + Build.VERSION.SDK_INT);
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+            if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+                Log.i(LOG_TAG, "Requesting for permission: WRITE_EXTERNAL_STORAGE");
+
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSIONS_REQUEST_PHOTO);
+            } else {
+                Log.i(LOG_TAG, "Permission WRITE_EXTERNAL_STORAGE granted");
+                dispatchPhotoIntent();
+            }
+        }
+        else {
+            dispatchPhotoIntent();
+        }
+    }
+
+    private void dispatchPhotoIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if (takePictureIntent.resolveActivity(getPackageManager()) == null) {
+            Log.e(LOG_TAG, "Can't take a photo. App not found.");
+            Log.e(LOG_TAG, "Please install photo app.");
+        }
+        else {
+            try {
+                Log.i(LOG_TAG, "Photo app found, starting activity...");
+                File photoFile = getPhotoFile();
+                Uri photoUri = FileProvider.getUriForFile(this, "org.chyla.photoapp.fileprovider", photoFile);
+
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+
+                Log.d(LOG_TAG, "Starting photo activity with uri: " + photoUri.toString());
+
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            } catch (IOException ex) {
+                Log.e(LOG_TAG, "Error while creating photo file: " + ex.getMessage());
+            }
+        }
+    }
+
+    private File getPhotoFile() throws IOException {
+        final String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+        final String imageFileName = "PhotoApp_" + timeStamp + "_";
+
+        final File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        final File photoFile = File.createTempFile(imageFileName, ".jpg", storageDir);
+
+        Log.d(LOG_TAG, "Temporary photo file: " + photoFile.toString());
+
+        return photoFile;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d(LOG_TAG, "onActivityResult called...");
+
+        if (resultCode == RESULT_OK && requestCode == REQUEST_IMAGE_CAPTURE) {
+            Log.d(LOG_TAG, "Succesfully returned from Photo app.");
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_PHOTO:
+                if ((grantResults.length > 0) && (grantResults[0] == PackageManager.PERMISSION_GRANTED))
+                    dispatchPhotoIntent();
+                break;
+
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+                break;
+        }
     }
 
     private void showGalleryFragment() {

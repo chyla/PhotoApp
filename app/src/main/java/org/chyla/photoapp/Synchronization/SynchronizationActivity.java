a0ee86f2b988f9fc5f8c4d2cba27bfbf4f74e066
Adam Chyła <adam@chyla.org>
2017-07-14 05:49:51 +0200

    remove unused imports

--- app/src/main/java/org/chyla/photoapp/Synchronization/SynchronizationActivity.java
+++ app/src/main/java/org/chyla/photoapp/Synchronization/SynchronizationActivity.java
@@ -5,22 +5,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import org.chyla.photoapp.Main.MainActivity;
-import org.chyla.photoapp.Model.Authenticator.Authenticator;
-import org.chyla.photoapp.Model.Authenticator.AuthenticatorImpl;
 import org.chyla.photoapp.R;
-import org.chyla.photoapp.Repository.CloudDatabase.CloudDatabaseRepository;
-import org.chyla.photoapp.Repository.CloudDatabase.Firebase.FirebaseRepository;
-import org.chyla.photoapp.Repository.LocalDatabase.DatabaseRepository;
-import org.chyla.photoapp.Repository.LocalDatabase.GreenDao.GreenDaoRepository;
-import org.chyla.photoapp.Repository.Login.LoginRepository;
-import org.chyla.photoapp.Repository.Login.LoginRepositoryImpl;
 import org.chyla.photoapp.Synchronization.DependencyInjection.DaggerSynchronizationComponent;
 import org.chyla.photoapp.Synchronization.DependencyInjection.SynchronizationComponent;
 import org.chyla.photoapp.Synchronization.DependencyInjection.SynchronizationModule;
-import org.chyla.photoapp.Synchronization.Model.SynchronizeDataInteractor;
-import org.chyla.photoapp.Synchronization.Model.SynchronizeDataInteractorImpl;
 import org.chyla.photoapp.Synchronization.Presenter.SynchronizationPresenter;
-import org.chyla.photoapp.Synchronization.Presenter.SynchronizationPresenterImpl;
 
 public class SynchronizationActivity extends AppCompatActivity implements View {
 
